2933c85bbb98d927eb70afaf1771352fc5ba0f2e
Jason <jasonliu6700@gmail.com>
2023-11-04 10:17:19 -0700

    Second commit

--- src/main/java/com/demo/selenium_automation/App.java
+++ src/main/java/com/demo/selenium_automation/App.java
@@ -1,5 +1,7 @@
 package com.demo.selenium_automation;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,9 +17,14 @@ public class App
     {
     	ChromeOptions chromeOptions = new ChromeOptions();
         WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("http://facebook.com");
-        driver.findElement(By.id("email")).sendKeys("myEmail");
-        driver.findElement(By.id("pass")).sendKeys("myPassword");
+        driver.get("http://54.177.205.97:8082/contact.html");
+        driver.findElement(By.id("inputName")).sendKeys("Qilin Liu");
+        driver.findElement(By.id("inputNumber")).sendKeys("4086200006");
+        driver.findElement(By.id("inputMail")).sendKeys("qilin.liu@tcs.com");
+        driver.findElement(By.id("inputMessage")).sendKeys("message");
+        driver.findElement(By.id("my-button")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        System.out.println(driver.findElement(By.id("response")).getText().equals("Message Sent"));
         driver.close();
     }
 }
--- src/test/java/com/demo/selenium_automation/AppTest.java
+++ src/test/java/com/demo/selenium_automation/AppTest.java
@@ -2,7 +2,13 @@ package com.demo.selenium_automation;
 
 import static org.junit.Assert.assertTrue;
 
+import java.time.Duration;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Unit test for simple App.
@@ -16,5 +22,20 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    
+    @Test
+    public void insureMeTest() throws InterruptedException {
+    	ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("http://54.177.205.97:8082/contact.html");
+        driver.findElement(By.id("inputName")).sendKeys("Qilin Liu");
+        driver.findElement(By.id("inputNumber")).sendKeys("4086200006");
+        driver.findElement(By.id("inputMail")).sendKeys("qilin.liu@tcs.com");
+        driver.findElement(By.id("inputMessage")).sendKeys("message");
+        driver.findElement(By.id("my-button")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        assertTrue(driver.findElement(By.id("response")).getText().equals("Message Sent"));
+        driver.close();
     }
 }
